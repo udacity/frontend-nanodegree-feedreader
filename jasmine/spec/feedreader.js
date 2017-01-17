1e9a35b00fdbf8460ac585e9ecf85d41e8447778
Richard Yhip <richard.yhip@gmail.com>
2017-01-17 11:11:18 -0500

    Update feedreader.js
    Updated comment for last test to not be open-ended

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -63,7 +63,7 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
