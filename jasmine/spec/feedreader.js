a31078a7d0bd46642dcad15da3562ba953ce080b
yanisleidi <yaroga88@yahoo.com>
2018-01-26 14:44:11 -0800

    Write new suite test named Initial Entries

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,6 +83,9 @@ $(function() {
 
     });
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
+    });
 
     /* TODO: Write a test that ensures when the loadFeed
      * function is called and completes its work, there is at least
