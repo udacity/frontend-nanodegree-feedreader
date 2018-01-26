a360fa23970c3f741387c1a60ecebf67b7ef4cf9
yanisleidi <yaroga88@yahoo.com>
2018-01-26 15:28:28 -0800

    Ensures when the loadFeed function is called and completes its work, there is at least an entry element within the feed container

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,15 +84,25 @@ $(function() {
     });
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
+        var feedContainer = $('.feed');
+        beforeEach(function(done) {
+            loadFeed(3, done);
+        });
+        afterEach(function() {
+            loadFeed(0);
+        });
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        it('there is at least one entry element', function(done) {
+            expect(feedContainer.children().length).toBeGreaterThan(0);
+            done();
+        });
 
     });
-
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
