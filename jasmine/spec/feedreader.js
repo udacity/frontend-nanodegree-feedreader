1666d43603679b1bf997a5a2c9b390c3e7479c6d
Chris Camp <cpcamp2@gmail.com>
2018-05-04 17:31:02 -0400

    add test comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -22,7 +22,8 @@ $(function() {
      * page?
      */
 
-    it('are defined', function() {
+    // Test to see if feeds are defined and not equal to zero
+    it('feeds are defined', function() {
         expect(allFeeds).toBeDefined();
         expect(allFeeds.length).not.toBe(0);
     });
@@ -32,6 +33,7 @@ $(function() {
      * and that the URL is not empty.
      */
 
+     //Test to see if feeds have a defined url and that the ural is not empty
      it('feeds have defined url and url is not empty', function(){
        for(let index in allFeeds) {
          expect(allFeeds[index].url).toBeDefined();
@@ -44,6 +46,7 @@ $(function() {
      * and that the name is not empty.
      */
 
+     // Test to see if feeds have defined name and is not empty
      it('feeds have defined name and name is not empty', function(){
        for(let index in allFeeds) {
          expect(allFeeds[index].name).toBeDefined();
@@ -61,6 +64,8 @@ $(function() {
      * the CSS to determine how we're performing the
      * hiding/showing of the menu element.
      */
+
+     // Test to see that menu element is hidden by default using jQuery
      it('menu element is hidden by default', function(){
        expect($('body').hasClass('menu-hidden')).toEqual(true);
      });
@@ -69,13 +74,20 @@ $(function() {
       * should have two expectations: does the menu display when
       * clicked and does it hide when clicked again.
       */
+
+      // Test to see if menu changes visibility when menu icon is clicked
       it('menu changes visibility when menu icon is clicked', function(){
+
+        // Create clicked function using jQuery to call for a click
         function clicked() {
           $('.menu-icon-link').click();
         }
 
+        // Call clicked and see if the menu toggles to being visible
         clicked();
         expect($('body').hasClass('menu-hidden')).toEqual(false);
+
+        // Call clicked and see if the menu toggles to being hidden
         clicked();
         expect($('body').hasClass('menu-hidden')).toEqual(true);
       });
@@ -90,10 +102,13 @@ $(function() {
      * Remember, loadFeed() is asynchronous so this test will require
      * the use of Jasmine's beforeEach and asynchronous done() function.
      */
+
+     // BeforeEach to load the feed
      beforeEach(function(done){
        loadFeed(0, done);
      });
 
+     // Check the entry to be sure that there is at least one
      it("loadFeed it completes it's work and there is at least a single entry in the feed container", function(){
        expect($('.entry').length).not.toBeLessThan(0);
      });
@@ -101,18 +116,24 @@ $(function() {
 
   /* TODO: Write a new test suite named "New Feed Selection" */
   describe('New Feed Selection', function(){
+    // Define new variable previousFeed to store first loaded feed
     let previousFeed;
     /* TODO: Write a test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
      */
+
+    //Before each to load the first feed and store it in previousFeed variable
     beforeEach(function(done){
       loadFeed(0, function() {
         previousFeed = $('.feed').html();
+
+        //Load the current feed to compare to the previous feed
         loadFeed(1, done);
       });
     });
 
+    // Check to be sure that the feeds do not match.
     it('feed to be different from previous feed', function(){
       expect($('.feed').html()).not.toBe(previousFeed);
     });
