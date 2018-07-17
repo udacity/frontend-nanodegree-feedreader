2bf7e0d37466174a39bdfd080d3c3dc8dc08aeb9
jdmedlock <jdmedlock@gmail.com>
2018-07-17 18:10:35 -0500

    Feature: Setup gulpfile.js to build the app
    Setup gulpfile.js to build the app
    
    Resolves: N/a
    See also: N/a

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+/*eslint-env node */
+
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+var eslint = require('gulp-eslint');
+var jasmine = require('gulp-jasmine-phantom');
+var concat = require('gulp-concat');
+
+gulp.task('default', ['copy-html', 'styles', 'lint', 'scripts'], function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+	gulp.watch('/index.html', ['copy-html']);
+	gulp.watch('./dist/index.html').on('change', browserSync.reload);
+
+	browserSync.init({
+		server: './dist'
+	});
+});
+
+// Prepare the app for Production Release
+gulp.task('dist', [
+	'copy-html',
+	'styles',
+	'lint',
+	'scripts-dist'
+]);
+
+// Prepare Javascript source for development testing
+gulp.task('scripts', function() {
+	gulp.src('js/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+// Prepare Javascript source for release to production
+gulp.task('scripts-dist', function() {
+	gulp.src('js/**/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy-html', function() {
+  // Copy index.html to the dist directory
+	gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', function() {
+  gulp.src('css/**/*.css')
+    // Update CSS prefixes to support the most recent two versions of
+    // each browser
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+    }))
+    // Output the CSS to the dist/css directory
+    .pipe(gulp.dest('dist/css'))
+    // Synchronize the browser with the latest set of changes
+		.pipe(browserSync.stream());
+});
+
+gulp.task('lint', function () {
+	return gulp.src(['js/**/*.js'])
+		// eslint() attaches the lint output to the eslint property
+		// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failOnError last.
+		.pipe(eslint.failOnError());
+});
+
+gulp.task('tests', function () {
+	gulp.src('./jasmine/spec/feedreader.js')
+		.pipe(jasmine({
+      verbose: true,
+      keepRunner: './',
+			integration: true,
+			vendor: 'js/**/*.js'
+		}));
+});
