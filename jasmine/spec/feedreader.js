ef898891c890f5b95b4eb6bd085591199416f4a0
hollywoodno <hollywoodno@gmail.com>
2018-07-22 18:36:29 -0700

    write menu test suite - side menu opens

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,18 +53,23 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('slide menu starts open', function() {
+            const body = document.querySelector('body');
+            expect(body.classList).toContain('menu-hidden');
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
