dccbc0f0c89fde076ae57acc4eb47799c6a10467
hollywoodno <hollywoodno@gmail.com>
2018-07-23 18:32:58 -0700

    code cleanup

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function () {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function () {
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +21,7 @@ $(function () {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function () {
+        it('are defined', () => {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -31,7 +31,7 @@ $(function () {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('has defined non-empty urls', function () {
+        it('has defined non-empty urls', () => {
             allFeeds.forEach(f => {
                 expect(f.url).toBeDefined();
                 expect(f.url).not.toBe('');
@@ -53,7 +53,7 @@ $(function () {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function () {
+    describe('The menu', () => {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
@@ -62,7 +62,7 @@ $(function () {
 
         const body = document.querySelector('body');
 
-        it('starts open', function () {
+        it('starts open', () => {
             expect(body.classList).toContain('menu-hidden');
         });
 
@@ -71,7 +71,7 @@ $(function () {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('icon toggles menu display', function () {
+        it('icon toggles menu display', () => {
             const menu = document.querySelector('.menu-icon-link');
 
             menu.click();
@@ -83,7 +83,7 @@ $(function () {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe('Initial Entries', function () {
+    describe('Initial Entries', () => {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -91,13 +91,13 @@ $(function () {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-        beforeEach(function (done) {
-            loadFeed(0, function () {
+        beforeEach( (done) => {
+            loadFeed(0, () => {
                 done()
             });
         });
 
-        it('has at least one entry', function (done) {
+        it('has at least one entry', (done) => {
             const container = $('.feed');
             expect(container).not.toBeNull();
 
@@ -109,27 +109,26 @@ $(function () {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function () {
+    describe('New Feed Selection', (done) => {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
 
         const container = $('.feed');
-        let feed;
-        let event;
+        let feed, event;
         
-        const checkLinks = function(evt) {
+        const checkLinks = (evt) => {
             evt.preventDefault();
             event = evt;
         }
 
-        beforeEach(function (done) {
+        beforeEach((done) => {
             
             // Capture clicking of a link
             $(document).on('click', 'a', checkLinks);
             
-            loadFeed(0, function () {
+            loadFeed(0, () => {
                 
                 // trigger click on feed
                 feed = $('.feed a')[0];
@@ -139,14 +138,14 @@ $(function () {
             });
         });
 
-        afterEach(function(done) {
+        afterEach( (done) => {
             $(document).off('click', 'a', checkLinks);
             done();
         });
 
-        it('is loaded', function (done) {
+        it('is loaded', (done) => {
 
-            // ensure that window location will change changed
+            // ensure that window location will change
             expect(event.target.href).toBe('http://blog.udacity.com/2018/07/this-former-engineer-landed-his-dream-career-as-a-data-scientist.html');
             expect(event.target.href).not.toBe('http://127.0.0.1:5500/index.html#');
             
