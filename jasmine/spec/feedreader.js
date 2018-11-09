8db42d4f0945e4fda2b42c7620ad9838579181a1
Gabriel Wu <qqbbnease1004@126.com>
2018-11-09 20:53:30 +0800

    test: every feed should have non-empty URL

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('has non-empty URL', function() {
+            allFeeds.map(feed => {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            })
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
